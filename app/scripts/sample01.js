82d384391ee8da8c3f96e5388dd537c70e00416b
Omar Sainz <SQL_Slammer.exe@live.de>
2015-04-10 00:06:09 -0500

    Design modified for height in several pages for responsive

--- app/scripts/sample01.js
+++ app/scripts/sample01.js
@@ -1,17 +1,35 @@
 ﻿'use strict'; 
 
 $(document).ready(function(){
-	$('.carousel').carousel({
-		hAlign: 'center', 
-		vAlign: 'center', 
-		frontWidth:350, 
-		frontHeight:223, 
-		carouselWidth:900,
-		carouselHeight:330,
-		directionNav:true,
-		shadow:false, 
-		buttonNav:'bullets', 
-		autoplay:false, 
-		reflection:false
-	});
+	if ($(window).width() > 1023) { 
+		$('.carousel').carousel({
+			hAlign: 'center', 
+			vAlign: 'center', 
+			frontWidth:350, 
+			frontHeight:223, 
+			carouselWidth:900,
+			carouselHeight:330,
+			directionNav:true,
+			shadow:false, 
+			buttonNav:'bullets', 
+			autoplay:false, 
+			reflection:false
+		});
+	}
+	else {
+		$('.carousel').carousel({
+			hAlign: 'center', 
+			vAlign: 'center', 
+			frontWidth:150, 
+			frontHeight:123, 
+			carouselWidth:800,
+			carouselHeight:230,
+			directionNav:true,
+			shadow:false, 
+			buttonNav:'bullets', 
+			autoplay:false, 
+			reflection:false
+		});
+	}
+
 });
